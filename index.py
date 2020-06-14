01abaa7967c5726a3af8152c2361ab99d9d6b5ac
chrishontoir <chrishontoir@gmail.com>
2020-06-14 15:56:28 +0100

    feat: added a header with buttons

--- index.py
+++ index.py
@@ -3,20 +3,56 @@ import os
 os.system('clear')
 
 root = Tk()
-root.title('Index')
+root.title('UE App')
 root.geometry('1000x600')
+root.configure(background='#272d36')
 
-def hello():
-    hello_label = Label(root, text='Hello ' + myTextbox.get())
-    hello_label.pack()
+header = Frame(root, height=80, background='#212730')
+header.pack(side=TOP, fill=X)
+header.pack_propagate(0)
 
-myLabel = Label(root, text='Enter your first name:')
-myLabel.pack()
+logo = Label(header, padx=10, text='UE', font=('Helvetica Neue', 40, 'bold'), fg='white', bg='#212730')
+logo.pack(side=LEFT)
 
-myTextbox = Entry(root, width=30)
-myTextbox.pack()
+menu = Frame(header, height=80, width=500, background='#212730')
+menu.pack(side=LEFT)
+menu.pack_propagate(0)
 
-myButton = Button(root, text='Submit', command=hello)
-myButton.pack()
+home = Label(menu, text='HOME', padx=10, background='#212730', font='bold', fg='white', cursor='hand')
+home.pack(side=LEFT, fill=Y)
+
+home.bind('<Enter>', lambda event, h=home: h.configure(bg='#272d36'))
+home.bind('<Leave>', lambda event, h=home: h.configure(bg='#212730'))
+
+roster = Label(menu, text='ROSTER', padx=10, background='#212730', font='bold', fg='white', cursor='hand')
+roster.pack(side=LEFT, fill=Y)
+
+roster.bind('<Enter>', lambda event, r=roster: r.configure(bg='#272d36'))
+roster.bind('<Leave>', lambda event, r=roster: r.configure(bg='#212730'))
+
+applications = Label(menu, text='APPLICATIONS', padx=10, background='#212730', font='bold', fg='white', cursor='hand')
+applications.pack(side=LEFT, fill=Y)
+
+applications.bind('<Enter>', lambda event, label=applications: label.configure(bg='#272d36'))
+applications.bind('<Leave>', lambda event, label=applications: label.configure(bg='#212730'))
+
+user = Frame(header, height=80, width=200, background='#212730')
+user.pack(side=RIGHT)
+user.pack_propagate(0)
+
+register = Label(user, text='REGISTER', padx=10, background='#212730', font='bold', fg='white', cursor='hand')
+register.pack(side=RIGHT, fill=Y)
+
+register.bind('<Enter>', lambda event, label=register: label.configure(bg='#272d36'))
+register.bind('<Leave>', lambda event, label=register: label.configure(bg='#212730'))
+
+login = Label(user, text='LOGIN', padx=10, background='#212730', font='bold', fg='white', cursor='hand')
+login.pack(side=RIGHT, fill=Y)
+
+login.bind('<Enter>', lambda event, label=login: label.configure(bg='#272d36'))
+login.bind('<Leave>', lambda event, label=login: label.configure(bg='#212730'))
+
+
+
 
 root.mainloop()
